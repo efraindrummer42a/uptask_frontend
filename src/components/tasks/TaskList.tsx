5e9c012e9e8d35a74ac3b73b446b50401a30d52f
efraindrummer42a <may.efrain@sycec.com.mx>
2024-08-14 16:38:29 -0600

    compoent tailwindc

--- src/components/tasks/TaskList.tsx
+++ src/components/tasks/TaskList.tsx
@@ -48,9 +48,9 @@ export default function TaskList({ tasks }: TaskListProps) {
 
         <div className='flex gap-5 overflow-x-scroll 2xl:overflow-auto pb-32'>
             {Object.entries(groupedTasks).map(([status, tasks]) => (
-                <div key={status} className='min-w-[300px] 2xl:min-w-0 2xl:w-1/5'>
+                <div key={status} className='min-w-[300px] font-bold 2xl:min-w-0 2xl:w-1/5'>
                     <h3 
-                        className={`capitalize text-xl font-light border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
+                        className={`capitalize text-xl font-bold border border-slate-300 bg-white p-3 border-t-8 ${statusStyles[status]}`}
                     >{statusTranslations[status]}</h3>
                     <ul className='mt-5 space-y-5'>
                         {tasks.length === 0 ? (
